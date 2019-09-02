f09826ed0ae6f2dcca632320d56c8c150ebdd0c1
Zuku271 <adrian.zuchowski@outlook.com>
2019-09-02 17:55:10 +0200

    Last changes before returning to config text file.

---
+++ src/paczka/Page.java
@@ -0,0 +1,43 @@
+package paczka;
+
+import java.util.HashMap;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+"startPage"
+})
+public class Page {
+
+@JsonProperty("startPage")
+private String startPage;
+@JsonIgnore
+private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+@JsonProperty("startPage")
+public String getStartPage() {
+return startPage;
+}
+
+@JsonProperty("startPage")
+public void setStartPage(String startPage) {
+this.startPage = startPage;
+}
+
+@JsonAnyGetter
+public Map<String, Object> getAdditionalProperties() {
+return this.additionalProperties;
+}
+
+@JsonAnySetter
+public void setAdditionalProperty(String name, Object value) {
+this.additionalProperties.put(name, value);
+}
+
+}
